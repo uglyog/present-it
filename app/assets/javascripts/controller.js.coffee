eb2ed46585d6353f08d9d01eef70c9b73eae83c2
Ronald Holshausen <uglyog@gmail.com>
2013-06-02 15:27:36 +1000

    got communication bus working between the controller and the presentation

--- app/assets/javascripts/controller.js.coffee
+++ app/assets/javascripts/controller.js.coffee
@@ -1,3 +1,13 @@
 #= require underscore
 #= require jquery
 #= require bootstrap
+
+ws = new WebSocket("ws://localhost:8080")
+ws.onmessage = (evt) ->
+  console.log "C Received: " + evt.data
+ws.onclose = (event) -> console.log("C Closed - code: " + event.code + ", reason: " + event.reason + ", wasClean: " + event.wasClean)
+ws.onopen = ->
+  console.log "C connected..."
+  ws.send("Hi I'm Controller")
+  ws.send("Presentation: Helloooooooo!!!!!")
+  
---
+++ channel.rb
@@ -0,0 +1,39 @@
+require 'em-websocket'
+require 'ap'
+
+EM.run do
+  connections = {}
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
+
+    ws.onopen do |handshake|
+      puts "WebSocket opened #{{
+        :path => handshake.path,
+        :query => handshake.query,
+        :origin => handshake.origin
+      }}"
+
+      ws.send "Hello Client!"
+    end
+
+    ws.onmessage do |msg|
+      match = msg.match /Hi I'm (\w*)/
+      if match
+        connections[match[1]] = ws
+        ap connections
+      end
+
+      match = msg.match /(\w*): (.*)/
+      if match
+        connections[match[1]].send(match[2])
+      end
+    end
+    ws.onclose do
+      connections.delete_if { |k, v| v == ws }
+      ap connections
+    end
+    ws.onerror do |e|
+      puts "Error: #{e.message}"
+      connections.delete_if { |k, v| v == ws }
+    end
+  end
+end
